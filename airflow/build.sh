1feb6c78caf3a5c766241a840a382899768d7a1f
Duy Nguyễn <duynguyen@Duys-MacBook-Air.local>
2024-11-06 18:32:19 +0700

    init

---
+++ airflow/build.sh
@@ -0,0 +1,2 @@
+# docker buildx build --platform linux/amd64,linux/arm64 -t duynguyenngoc/airflow:slim-2.10.2-python3.12-v1.0.3 --push .
+docker buildx build --platform linux/amd64 -t duynguyenngoc/airflow:crypto-monitoring --push .
